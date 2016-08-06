3a1dbb278134efa6a450079f53de5354229e102c
Vaclav Petras <wenzeslaus@gmail.com>
2016-08-06 12:12:41 -0400

    initial content taken from BH FOSS4G NA presentation without videos and with smaller and optimized images

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 ./copy-common-files.py --dst-dir=build
 
 ./build-slides.py --outdir=build \
-    title.html template-slides.html plain-slides.html \
-    math-slides.html build-slides.html
+    --outfile=foss4g-na-2016.html \
+    foss4g-na-2016.html
+
+./build-slides.py --outdir=build \
+    --outfile=geoforall-webinar.html \
+    geoforall-webinar.html
+
+# make this presentation primary for convenience
+cp build/geoforall-webinar.html build/index.html
