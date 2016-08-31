8bdf2276933f98da9025fcb766d1e28bb22d14d1
Vaclav Petras <wenzeslaus@gmail.com>
2016-08-31 19:58:54 -0400

    improve HTML metadata

--- build.sh
+++ build.sh
@@ -2,10 +2,14 @@
 
 ./build-slides.py --outdir=build \
     --outfile=foss4g-na-2016.html \
+    --title="Tangible Landscape" \
+    --meta-description="Clone of slides for Tangible Landscape at FOSS4G NA" \
     foss4g-na-2016.html
 
 ./build-slides.py --outdir=build \
     --outfile=geoforall-webinar.html \
+    --title="Tangible Landscape Webinar" \
+    --meta-description="Slides for Tangible Landscape Webinar (GeoForAll, UCGIS, ASPRS)" \
     geoforall-webinar.html
 
 # make this presentation primary for convenience
