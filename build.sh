c82e85a429bace61cf21b3a5e3aaf2282ce7f701
payam tabrizian <ptabriz@ncsu.edu>
2017-05-30 17:18:12 -0400

    updated build file

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@
     ncaug-symposium.html
 
 cp ncgis2017.html ./build/
-cp EDRA2017.html ./build/
+cp EDRA_2017.html ./build/
 
 # make this presentation primary for convenience
 cp build/geoforall-webinar.html build/index.html
